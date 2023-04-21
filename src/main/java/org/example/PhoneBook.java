808bda604a14a68e06aac961920ad6877bf3cf41
axer665 <vellsevul@mail.ru>
2023-04-21 09:43:10 +0300

    Stub and tests for add

--- src/main/java/org/example/PhoneBook.java
+++ src/main/java/org/example/PhoneBook.java
@@ -1,4 +1,14 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class PhoneBook {
+    Map<String, String> guide = new HashMap<>();
+    public int add(String name, String number) {
+        return 0;
+    }
+
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,3 +1,42 @@
-public class PhoneBookTest {
+import org.example.PhoneBook;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
+public class PhoneBookTest {
+    PhoneBook sut = new PhoneBook();
+    @Test
+    public void testsAdditional() {
+        System.out.println("Test add");
+
+        // given:
+        String name = "Вася";
+        String number = "9067697727";
+
+        int expected = 1;
+
+        // when:
+        var result = sut.add(name, number);
+
+        // then:
+        Assertions.assertEquals(expected, result);
+    }
+
+    @Test
+    public void testsAdditionalDouble() {
+        System.out.println("Test add double");
+
+        // given:
+        String name1 = "Вася";
+        String name2 = "Лена";
+        String number = "9067697727";
+
+        int expected = 1;
+
+        // when:
+        sut.add(name1, number); // Добавляем пользователя
+        var result = sut.add(name2, number); // Пользователь с одинаковым номером не должен добавиться
+
+        // then:
+        Assertions.assertEquals(expected, result);
+    }
 }
